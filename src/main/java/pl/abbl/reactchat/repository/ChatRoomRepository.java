a9b95ac0b0ec0d7b06e2165e85ddddf21bc6d890
abbl <patrykrzucidlo1@gmail.com>
2018-10-10 07:21:47 +0200

    Renamed class

--- src/main/java/pl/abbl/reactchat/repository/ChatRoomRepository.java
+++ src/main/java/pl/abbl/reactchat/repository/ChatRoomRepository.java
@@ -3,6 +3,6 @@ package pl.abbl.reactchat.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.abbl.reactchat.entity.ChatRoom;
 
-public interface ChatRoomsRepository extends JpaRepository<ChatRoom, Integer> {
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Integer> {
 
 }
--- src/main/java/pl/abbl/reactchat/service/impl/ChatRoomsServiceImpl.java
+++ src/main/java/pl/abbl/reactchat/service/impl/ChatRoomsServiceImpl.java
@@ -7,21 +7,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import pl.abbl.reactchat.entity.ChatRoom;
-import pl.abbl.reactchat.repository.ChatRoomsRepository;
+import pl.abbl.reactchat.repository.ChatRoomRepository;
 import pl.abbl.reactchat.repository.enums.ChatRoomType;
 import pl.abbl.reactchat.service.ChatRoomsService;
 
 @Service
 public class ChatRoomsServiceImpl implements ChatRoomsService{
 	@Autowired
-	private ChatRoomsRepository chatRoomsRepository;
+	private ChatRoomRepository chatRoomRepository;
 
 
 	@Override
 	public List<ChatRoom> getPublicChatRooms() {
 		List<ChatRoom> publicChatRooms = new ArrayList<>();
 
-		for(ChatRoom chatRoom : chatRoomsRepository.findAll()){
+		for(ChatRoom chatRoom : chatRoomRepository.findAll()){
 			if(chatRoom.getType() == ChatRoomType.PUBLIC){
 				publicChatRooms.add(chatRoom);
 			}
