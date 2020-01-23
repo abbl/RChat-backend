7a3f823e6acf3fe376effb58adef04401d491fcc
Patrick <patrykrzucidlo1@gmail.com>
2020-01-23 15:58:40 +0100

    Updated AuthenticationService to use RefreshTokenService

--- src/app/services/AuthenticationService.ts
+++ src/app/services/AuthenticationService.ts
@@ -1,12 +1,10 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { Inject, Service } from 'typedi';
-import { getConnection, Repository } from 'typeorm';
-import uuid from 'uuid';
 import AuthenticationResult from '../authentication/AuthenticationResult';
 import { UserTokenContent } from '../authentication/UserTokenContent';
-import RefreshToken from '../models/RefreshToken';
 import User from '../models/User';
+import RefreshTokenService from './RefreshTokenService';
 import UserService from './UserService';
 
 @Service()
@@ -14,9 +12,10 @@ export default class AuthenticationService {
     @Inject()
     private userService: UserService;
 
-    private tokenSecret: string = process.env.API_SECRET;
+    @Inject()
+    private refreshTokenService: RefreshTokenService;
 
-    private refreshTokenRepository: Repository<RefreshToken> = getConnection().getRepository(RefreshToken);
+    private tokenSecret: string = process.env.API_SECRET;
 
     /**
      * After successful authentication returns JWT and RefreshToken.
@@ -25,14 +24,11 @@ export default class AuthenticationService {
     public async signIn(username: string, password: string): Promise<AuthenticationResult | null> {
         const user = await this.userService.findOneByUsername(username);
 
-        console.log(user);
-
         if (user) {
             const isPasswordCorrect = await bcrypt.compare(password, user.password);
 
             if (isPasswordCorrect) {
-                const authorizationKeys = this.createAuthorizationKeys(username, user.role);
-                await this.saveRefreshToken(authorizationKeys.refreshToken, user);
+                const authorizationKeys = this.createAuthorizationKeys(user);
 
                 return authorizationKeys;
             }
@@ -41,43 +37,16 @@ export default class AuthenticationService {
         return null;
     }
 
-    private async saveRefreshToken(refreshToken: string, user: User): Promise<void> {
-        const refreshTokenEntity: RefreshToken = {
-            token: refreshToken,
-            belongsTo: user,
-            expiresAt: this.calculateTokenExpirationDay(7),
+    private async createAuthorizationKeys(user: User): Promise<AuthenticationResult> {
+        return {
+            authenticationToken: this.createJWT(user.username, user.role),
+            refreshToken: (await this.refreshTokenService.createRefreshToken(user)).token,
         };
-
-        await this.refreshTokenRepository.save(refreshTokenEntity);
-    }
-
-    private async checkIfPreviousTokenExist(user: User): Promise<boolean> {
-        const previousToken = await this.refreshTokenRepository.findOne({ where: { belongsTo: user } });
-
-        console.log(previousToken);
-
-        return false;
-    }
-
-    private calculateTokenExpirationDay(expiresIn: number) {
-        let expirationDate = new Date(Date.now());
-
-        expirationDate.setDate(expirationDate.getDate() + expiresIn);
-
-        return expirationDate;
-    }
-
-    private createAuthorizationKeys(username: string, role: string): AuthenticationResult {
-        return { authenticationToken: this.createJWT(username, role), refreshToken: this.createRefreshToken() };
     }
 
     private createJWT(username: string, role: string): string {
         const userTokenContent: UserTokenContent = { username: username, role: role };
 
         return jwt.sign(userTokenContent, this.tokenSecret, { expiresIn: '7d' });
-    }
-
-    private createRefreshToken(): string {
-        return uuid.v4();
     }
 }
